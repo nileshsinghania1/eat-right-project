aa052a836b250a6f601492aee692f9a939aa1058
nileshsinghania1 <67596557+nileshsinghania1@users.noreply.github.com>
2026-03-01 17:19:49 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,5 @@
-import { Navbar } from "@/components/Navbar";
-import { Hero } from "@/components/Hero";
-import { OfferSection } from "@/components/OfferSection";
-import { Ingredients } from "@/components/Ingredients";
-import { FAQ } from "@/components/FAQ";
-import { Footer } from "@/components/Footer";
+import ScrollStory from "@/components/ScrollStory";
 
 export default function Page() {
-  return (
-    <main>
-      <Navbar />
-      <Hero />
-      <OfferSection />
-      <Ingredients />
-      <FAQ />
-      <Footer />
-    </main>
-  );
+  return <ScrollStory />;
 }
